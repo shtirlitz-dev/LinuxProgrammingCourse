4ea4844c529e07025ac861e7ce0eac920725f822
Vsevolod Lukyanin <lukianin@mail.ru>
2023-07-25 20:07:56 +0000

    exit by timeout

--- p4_ipc/2multiplex/solution.c
+++ p4_ipc/2multiplex/solution.c
@@ -25,47 +25,28 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
 
     int sum = 0;
-    int ok1 = 1, ok2 = 1;
-    while(ok1 || ok2)
+    while(1)
     {
         fd_set read_fds;
         FD_ZERO(&read_fds);
-        if(ok1)
-            FD_SET(fd1, &read_fds);
-        if(ok2) 
-            FD_SET(fd2, &read_fds);
+        FD_SET(fd1, &read_fds);
+        FD_SET(fd2, &read_fds);
 
         struct timeval timeout;
-        timeout.tv_sec = 1; // Set the timeout in seconds
+        timeout.tv_sec = 8; // Set the timeout in seconds
         timeout.tv_usec = 0; // Set the timeout in microseconds (0 for no microseconds)
 
         int max_fd = (fd1 > fd2) ? fd1 : fd2;
         int ready_fds = select(max_fd + 1, &read_fds, NULL, NULL, &timeout);
+        if(!ready_fds)
+            break;
 
         if (FD_ISSET(fd1, &read_fds)) {
-            if(!update_sum(fd1, &sum))
-                ok1 = 0;
+            update_sum(fd1, &sum);
         }
         if (FD_ISSET(fd2, &read_fds)) {
-            if(!update_sum(fd2, &sum))
-                ok2 = 0;
+            update_sum(fd2, &sum);
         }
-            /*
-        if(ok1) {
-            int fd3 = open("in1", O_RDWR); // | O_NONBLOCK);
-            if(fd3 != -1)
-                close(fd3);
-            else
-                ok1 = 0;
-        }
-        if(ok2) {
-            int fd4 = open("in2", O_RDWR); // | O_NONBLOCK);
-            if(fd4 != -1)
-                close(fd4);
-            else
-                ok2 = 0;
-        }
-        */
     }
 
     printf("%d\n", sum);
--- p4_ipc/2multiplex/test.c
+++ p4_ipc/2multiplex/test.c
@@ -6,21 +6,25 @@
 
 int main(void)
 {
-    int fd1, fd2;
+    int fd1, fd2, n;
 
     mkfifo("in1", 0644);
     mkfifo("in2", 0644);
     fd1 = open("in1", O_WRONLY);
     fd2 = open("in2", O_WRONLY);
 
-    getchar();
+    scanf("%d", &n);
     write(fd1, "10", 2);
+    scanf("%d", &n);
     write(fd2, "20", 2);
-    getchar();
+    scanf("%d", &n);
     write(fd2, "4", 1);
+    scanf("%d", &n);
     write(fd1, "1", 1);
 
+    scanf("%d", &n);
     close(fd1);
+    scanf("%d", &n);
     close(fd2);
     unlink("in1");
     unlink("in2");

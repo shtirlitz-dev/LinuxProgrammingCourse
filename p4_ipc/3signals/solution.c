f487b6714703c0e63e5c56ba73e159172cedde81
Vsevolod Lukyanin <lukianin@mail.ru>
2023-07-30 14:22:03 +0200

    comments command line

--- p4_ipc/3signals/solution.c
+++ p4_ipc/3signals/solution.c
@@ -5,16 +5,19 @@
 
 int sig1 = 0, sig2 = 0;
 
+// command line: pkill -USR1 solution
 void handlerUsr1(int signo)
 {
     //printf("Called with %d\n", signo);
     sig1++;
 }
+// cmd line: pkill -USR2 solution
 void handlerUsr2(int signo)
 {
     //printf("Called with %d\n", signo);
     sig2++;
 }
+// cmd line: pkill -TERM solution
 void my_sig_handler(int signo)
 {
     printf("%d %d\n", sig1, sig2);
